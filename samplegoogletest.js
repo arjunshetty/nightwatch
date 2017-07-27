38f6706a808e15c863e403fdea883aa13bc97f80
U-CORP\ashetty <ashetty@D-SJC-00531094.corp.ebay.com>
2017-07-27 16:08:29 -0700

    latest changes

--- samplegoogletest.js
+++ samplegoogletest.js
@@ -4,10 +4,32 @@ module.exports = {
       .url('http://www.google.com')
       .waitForElementVisible('body', 1000)
       .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
+      .waitForElementVisible('button[name=btnG]', 3000)
+      .click('button[name=btnk]')
       .pause(1000)
       .assert.containsText('#main', 'Night Watch')
-      .end();
-  }
+	  .end();
+  },
+  'Search google keyword' : function (browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'ebay')
+      .waitForElementVisible('button[name=btnG]', 3000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('#main', 'ebay')
+	  .end();
+  },
+  'Search another keyword' : function (browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'netflix')
+      .waitForElementVisible('button[name=btnG]', 3000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('#main', 'netflix')
+	  .end();
+	  }
 };
